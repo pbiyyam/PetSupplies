0dbbba70044ec450d7df103afc26df79aef8e9dd
pbiyyam <smileypbveena@gmail.com>
2015-06-09 16:23:38 +0530

    added initial script files

---
+++ PetStoreAppS/petstorewebcommon/src/main/DbScripts/Admin.sql
@@ -0,0 +1,2 @@
+insert into user_details values(5,'345345435','sample@gmail.com','sdfsdfsds','sdfsdfsdf','827ccb0eea8a706c4c34a16891f84e7b','dfdfsadf',2);
+commit;
---
+++ PetStoreAppS/petstorewebcommon/src/main/DbScripts/Roles.sql
@@ -0,0 +1,3 @@
+insert into roles_details values(1,'USER');
+insert into roles_details values(2,'ADMIN');
+commit;
